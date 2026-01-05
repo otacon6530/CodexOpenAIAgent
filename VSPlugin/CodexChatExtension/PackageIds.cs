e06a3c15d252bbc1f9cb4b73c26d1aec2489a751
James Stephens <onlineworkmail@gmail.com>
2026-01-04 21:16:35 -0500

    removing visual studio plugin that didn't work

--- VSPlugin/CodexChatExtension/PackageIds.cs
+++
@@ -1,8 +0,0 @@
-namespace CodexChatExtension
-{
-    internal static class PackageIds
-    {
-        public const int CodexCommandGroup = 0x0100;
-        public const int OpenChatWindowCommand = 0x0101;
-    }
-}
